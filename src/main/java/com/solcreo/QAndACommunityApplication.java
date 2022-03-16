8a81bd1abaea24e9b577de67d7237c475db87ccd
Ryunosuke Fukuda <ryunosukecardiff@gmail.com>
2022-03-16 14:32:42 +0900

    first commit

---
+++ src/main/java/com/solcreo/QAndACommunityApplication.java
@@ -0,0 +1,13 @@
+package com.solcreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAndACommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAndACommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solcreo/QAndACommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solcreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAndACommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
